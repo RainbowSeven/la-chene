b886acd3030c170829ec0360714304b221964089
Oluwafemi Sule <thezurg07@gmail.com>
2015-07-04 14:40:58 +0100

    Remove upload in cv

--- app/config/database.php
+++ app/config/database.php
@@ -54,9 +54,9 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'lachene',
-			'username'  => 'root',
+			'host'      => '',
+			'database'  => '',
+			'username'  => '',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
