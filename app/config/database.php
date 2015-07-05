dc1f8acba99f0be7f521cd30fa7708050441d997
Oluwafemi Sule <thezurg07@gmail.com>
2015-07-05 19:15:05 +0100

    move db config to env

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'lachene',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => $_ENV['host'],
+			'database'  => $_ENV['database'],
+			'username'  => $_ENV['username'],
+			'password'  => $_ENV['password'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
