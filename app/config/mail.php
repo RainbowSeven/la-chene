d1fa474e345a5ec0b7a682ca85f3355bbe20e8fc
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-08-19 07:32:46 +0100

    Use sendmail

--- app/config/mail.php
+++ app/config/mail.php
@@ -106,7 +106,7 @@ return array(
     |
      */
 
-    'sendmail' => '/usr/sbin/sendmail -bm',
+    'sendmail' => '/usr/sbin/sendmail',
 
     /*
     |--------------------------------------------------------------------------
